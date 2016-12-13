f10ad57bfef482db0bd72f453a8f803c03874e60
Ryan Novotny <ryan.novotny@gmail.com>
2016-12-12 18:27:01 -0800

    add facebook pixel?

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ $( document ).ready(function() {
 		$('body').removeClass("spin")
 		this.offsetWidth = this.offsetWidth
 		$('body').addClass("spin")
+		fbq('track', 'AddToCart', {
+			content_ids: ['1234'],
+			content_type: 'product',
+			value: 2.99,
+			currency: 'USD'
+		})
 	})
 	
 	$('.draggable').draggable()
